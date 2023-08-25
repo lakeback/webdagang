09ab3ffce3b5e92a8b876fbd3c1cf6b282ac5284
lakeback <lakeback@qq.com>
2023-08-25 22:42:18 +0800

    commit dagangword grade name

--- dg/app01/wordtest/dagangword.py
+++ dg/app01/wordtest/dagangword.py
@@ -4,22 +4,30 @@ word_path = 'app01/wordtest/data/word list.xls'
 wb = xlrd.open_workbook(word_path)
 wb.sheet_names()
 sh = wb.sheet_by_name('latest')
+grade_name = {
+    '1.0': '七上',
+    '2.0': '七下',
+    '3.0': '八上',
+    '4.0': '八下',
+    '5.0': '九'
+}
 
 def word_Normal(grade = 1.0,unit = 1.0):
     word_in_rightgrade = {}
     word_out_grade = {}
     grade = float(grade)
     unit = float(unit)
+
     for rn in range(1, sh.nrows):
         word, _, word_mean, _, _, _, _,grd, unt, cls = sh.row_values(rn)
         if word.isalpha():
             word = word.lower()
         if grd < grade:
-            word_in_rightgrade[word] = [word, word_mean, grd, unt, cls]
+            word_in_rightgrade[word] = [word, word_mean, grade_name[str(grd)], unt, cls]
         elif grd == grade and unt <= unit:
-            word_in_rightgrade[word] = [word,word_mean,grd,unt,cls]
+            word_in_rightgrade[word] = [word,word_mean,grade_name[str(grd)],unt,cls]
         else:
-            word_out_grade[word] = [word,word_mean,grd,unt,cls]
+            word_out_grade[word] = [word,word_mean,grade_name[str(grd)],unt,cls]
     return word_in_rightgrade, word_out_grade
 
 
